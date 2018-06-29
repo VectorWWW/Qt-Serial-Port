c1cc24261fad375679b7fcf7eae1c53db75b3989
vector <jianzai2311@163.com>
2018-06-29 11:00:16 +0800

    串口通信框架

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,79 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initPort();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::readCom()
+{
+    QByteArray temp = serial.read(1);
+    if(!temp.isEmpty()){
+        ui->uartRecvText->insertPlainText("0x");
+        ui->uartRecvText->insertPlainText(temp.toHex());
+        ui->uartRecvText->insertPlainText(" ");
+    }
+}
+
+
+void MainWindow::on_uartOpenCloseBtn_clicked()
+{
+    if(ui->portNameCombo->isEnabled()){
+        ui->uartOpenCloseBtn->setText("Close");
+        ui->portNameCombo->setDisabled(true);
+        serial.setPortName(ui->portNameCombo->currentText());
+        serial.setBaudRate(QSerialPort::BaudRate(ui->baudRateCombo->currentText().toInt()), QSerialPort::AllDirections);
+        serial.setDataBits(QSerialPort::DataBits(ui->dataBitsCombo->currentText().toInt()));
+        serial.setFlowControl(QSerialPort::NoFlowControl);
+        serial.setParity(QSerialPort::Parity(ui->parityCombo->currentIndex() == 0 ?
+                                                 0 : ui->parityCombo->currentIndex() + 1));
+        serial.setStopBits(QSerialPort::StopBits(ui->stopBitsComb->currentText().toFloat() == 1.5 ?
+                                                    3 : ui->stopBitsComb->currentText().toInt()));
+        serial.close(); // 先关串口，再打开，可以保证串口不被其他函数占用
+        if(serial.open(QIODevice::ReadWrite)){
+            connect(&serial, SIGNAL(readyRead()), this, SLOT(readCom()));   // 把串口的readRead()信号绑定到readCom()这个槽函数上
+        }
+    }else{
+        ui->uartOpenCloseBtn->setText("Open");
+        ui->portNameCombo->setEnabled(true);
+        serial.close();
+    }
+}
+
+void MainWindow::on_uartSendBtn_clicked()
+{
+    serial.write(ui->uartSendText->toPlainText().toLatin1());   // 以ASCII码的形式通过串口发送出去
+}
+
+void MainWindow::initPort()
+{
+    foreach(const QSerialPortInfo &info, QSerialPortInfo::availablePorts()){
+       QSerialPort serialtmp;
+       serialtmp.setPort(info);
+       /* 判断端口是否能打开 */
+       if(serialtmp.open(QIODevice::ReadWrite))
+       {
+           int isHaveItemInList = 0;
+           /* 判断是不是已经在列表中了 */
+           for(int i=0; i<ui->portNameCombo->count(); i++)
+           {
+               /* 如果，已经在列表中了，那么不添加这一项了就 */
+               if(ui->portNameCombo->itemData(i) == serialtmp.portName())
+               {
+                   isHaveItemInList++;
+               }
+           }
+
+           if(isHaveItemInList == 0)
+           {
+               ui->portNameCombo->addItem(serialtmp.portName());
+           }
+           serialtmp.close();
+       }
+    }
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +18,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void readCom();
+    void on_uartOpenCloseBtn_clicked();
+
+    void on_uartSendBtn_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QSerialPort serial;
+
+    void initPort();
 };
 
 #endif // MAINWINDOW_H
